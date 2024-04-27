75c13c3f118a576034cb1aefa80a9282293508f6
sinedon <111921789+sinedon@users.noreply.github.com>
2024-04-26 21:36:04 -0400

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 1;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://guitar-services-latest.onrender.com";
+const host_remote = "https://guitar-serviceshw9-latest.onrender.com";
 
 function getHost() {
     return (mode === 0) ? host_local : host_remote;
